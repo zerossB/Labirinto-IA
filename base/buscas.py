8aad114e67821bb85c439ab2e38c0f042420fb92
Haynesss <zerossb@gmail.com>
2018-05-01 17:56:10 -0300

    Busca em Largura

--- base/buscas.py
+++ base/buscas.py
@@ -4,7 +4,7 @@ import numpy as np
 import base.funcoes as fn
 
 
-def buscaLargura(data, estado_pai, objective):
+def buscaLargura(data, estado_pai):
     cor = {}
     pred = {}
     d = {}
@@ -24,7 +24,7 @@ def buscaLargura(data, estado_pai, objective):
         u = Q.get()
 
         if u.goal:
-            return 0
+            return pred
 
         for v in u.children:
             if cor[v] == 'branco':
@@ -34,3 +34,37 @@ def buscaLargura(data, estado_pai, objective):
 
                 Q.put(v)
         cor[u] = 'preto'
+
+
+def buscaProfundidade(data, estado_pai):
+    cor = {}
+    pred = {}
+    d = {}
+
+    for v in fn.list_state(estado_pai, []):
+        d[v] = np.inf
+        cor[v] = 'branco'  # branco cinza e preto
+        pred[v] = None
+
+    cor[estado_pai] = 'cinza'
+    d[estado_pai] = 0
+
+    Q = LifoQueue()
+    Q.put(estado_pai)
+
+    while not Q.empty():
+        u = Q.get()
+
+        if u.goal:
+            return pred
+
+        for v in u.children:
+            if cor[v] == 'branco':
+                cor[v] = 'cinza'
+                d[v] = d[u] + 1
+                pred[v] = u
+
+                Q.put(v)
+        cor[u] = 'preto'
+
+    return pred
