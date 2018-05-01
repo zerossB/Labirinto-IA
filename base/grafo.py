5ba2f54d447b7a970b47e22d228ce9639e86d463
Haynesss <zerossb@gmail.com>
2018-05-01 14:55:38 -0300

    Criei a Aresta

--- base/grafo.py
+++ base/grafo.py
@@ -18,3 +18,39 @@ class Graph_state:
     def __str__(self):
         repre = "(%d,%d)" % (self.line, self.column)
         return repre
+
+
+class Aresta(object):
+    def __init__(self, g_ini=None, g_fim=None, custo=0):
+        self.g_ini = g_ini
+        self.g_fim = g_fim
+        self.custo = custo
+
+
+# def busca_largura(data, estado_pai):
+#     visitado = list()
+#     marcado = list()
+#     resultado = list()
+
+#     visitado.append(estado_pai)
+    
+#     if estado_pai in marcado:
+#         marcado.remove(estado_pai)
+#     print("Visitado: %s" % str(estado_pai))
+
+#     for estado_filho in estado_pai.children:
+#         if not estado_filho in marcado and not estado_filho in visitado:
+#             marcado.append(estado_filho)
+#             print("Marcado: %s" % str(estado_filho))
+
+#     for estado_filho in marcado:
+#         busca_largura(data, estado_filho)
+
+#     if estado_pai.goal:
+#         data[estado_pai.line][estado_pai.column] = [255, 0, 0, 255]
+#         resultado.append(estado_pai)
+
+#     for estado_filho in estado_pai.children:
+#         if estado_filho in resultado:
+#             data[estado_pai.line][estado_pai.column] = [255, 0, 0, 255]
+#             resultado.append(estado_pai)
