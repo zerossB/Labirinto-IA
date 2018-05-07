42e868b69da6bca1e6e9f776546a501d412076bf
sam753 <luciosam789@hootmail.com>
2018-05-07 18:32:11 -0300

    Comentarios sobre as buscas

--- base/buscas.py
+++ base/buscas.py
@@ -36,15 +36,18 @@ class Buscas(object):
 
 
 class BuscaLargura(Buscas):
+    # Variaveis que serão utilizadas durante a busca
     def __init__(self):
         super().__init__()
         self.cor = {}
         self.pred = {}
         self.d = {}
+        # Nome da busca
         self.name = "Busca Largura"
 
     def search(self, data, estado_pai):
-        for v in fn.list_state(estado_pai, []):
+        # 
+        for v in fn.list_state(estado_pai, []):             
             self.d[v] = np.inf
             self.cor[v] = 'branco'  # branco cinza e preto
             self.pred[v] = None
@@ -57,51 +60,68 @@ class BuscaLargura(Buscas):
         Q = Queue()
         Q.put(estado_pai)
 
+        # Verifica se tem algum estado pai na minha lista, caso tenha ele entra no while
         while not Q.empty():
             u = Q.get()
 
+        # Caso o atual "u" que é minha lista de estados pai, contenha o estado pai objetivo, então sai do while
             if u.goal:
                 break
 
+        # para cada filho na lista de filhos do pai a ser analisado
             for v in u.children:
+                # se sua cor for branca, eu troco ela pra cinza
                 if self.cor[v] == 'branco':
                     self.cor[v] = 'cinza'
+                    # Adiciono o meu filho atual a lista d[v .................................................................. arrumar isso]
                     self.d[v] = self.d[u] + 1
                     self.pred[v] = u
                     self.drawPoint(data, v, self.cor[v])
 
+                    # A minha lista de estados visitados recebe como seu parametro as listas U e V ..................................................
                     self.visitado.append((u, v))
 
                     Q.put(v)
             self.cor[u] = 'preto'
             self.drawPoint(data, u, self.cor[u])
 
+        # O resultado é
         self.resultado = [key for key in self.cor if self.cor[key] == 'preto']
+
+        # Salva uma imagem com os dados coletados nos passos anteriores e com os estados visitados pintados
         fn.save_image(data, "Resolucao-Largura.png")
 
 
 class BuscaProfundidade(Buscas):
+    # Variaveis que serão utilizadas durante a busca
     def __init__(self):
         super().__init__()
         self.cor = {}
         self.pred = {}
         self.d = {}
         self.f = {}
+        # Nome da busca
         self.name = "Busca Profundidade"
 
     def search(self, data, estado_pai):
+        # tempo inicial
         tempo = 0
 
+        # Para cada estado possivel a partir do estado pai, ele armazena estes estados em uma lista e os coloca todos como cor branca
         for v in fn.list_state(estado_pai, []):
             # cores possíveis: branco, cinza e preto
             self.cor[v] = 'branco'
             self.pred[v] = None
 
         for v in fn.list_state(estado_pai, []):
+            # para cada filho na lista, verifica-se se ele é branco
             if self.cor[v] == 'branco':
+                # se sim  ..................................
                 tempo = self.visit(estado_pai, v, tempo)
-
+        # 
         self.resultado = [key for key in self.cor if self.cor[key] == 'preto']
+
+
 
     def visit(self, G, s, tempo):
         tempo = tempo + 1
@@ -144,34 +164,43 @@ class BuscaCustoUniforme(Buscas):
     """
 
     def __init__(self):
+    # Variaveis que serão utilizadas durante a busca
         super().__init__()
         self.cor = {}
+        # Nome da busca
         self.name = "Busca Custo Uniforme"
 
     def geraResultado(self):
         self.resultado = [key for key in self.cor if self.cor[key] == 'preto']
 
+
     def search(self, data, estado_pai):
         frontier = PriorityQueue()
         frontier.put((0, estado_pai))
-
+        # se a fila dos estados pai não estiver vazia, entra no while
         while not frontier.empty():
             ucs_w, current_node = frontier.get()
+            #Pega o estado atual da minha lista "frontier", ao andar pega o estaddo atual e o coloca na lista de estados visitados
             self.visitado.append(current_node)
 
+            # Se o estado atual for o objetivo, finaliza a busca
             if current_node.goal:
                 # print("Cheguei no final! ", current_node)
                 return
 
+            # para cada estado filho
             for node in current_node.children:
+                # O custo dele é calculado com uma heuristica
                 custo = current_node.arestas[node].custoH
+                # ..............................................
                 filho = current_node.arestas[node].g_fim
+
+                # 
                 if not filho in self.visitado:
                     self.resultado.append((current_node, filho))
                     frontier.put(
                         (custo, filho)
                     )
-
 
 class BuscaGreedy(Buscas):
     """
@@ -196,26 +225,38 @@ class BuscaGreedy(Buscas):
     """
 
     def __init__(self):
+        # Variaveis que serão utilizadas durante a busca
         super().__init__()
         self.cor = {}
         self.H = {}
+        # Nome da busca
         self.name = "Busca Greedy (Gulosa)"
 
     def search(self, data, estado_pai):
         frontier = PriorityQueue()
+        #Adiciona dados a minha lista de estados pai
         frontier.put((0, estado_pai))
 
+        # Se a minha lista de estados pai não estiver vazia, entra no while
         while not frontier.empty():
             ucs_w, current_node = frontier.get_nowait()
+            # O estado atual a ser analizado é adcionado na lista de visitados
             self.visitado.append(current_node)
 
+            # Se o estado atual for o fim, finaliza o while
             if current_node.goal:
                 # print("Cheguei no final! ", current_node)
                 return
-
+            
+            # Para cada estado filho
             for node in current_node.children:
+                # Adiciona seu custo com uma busca heuristica
                 custo = current_node.arestas[node].custoH
+
+                # 
                 filho = current_node.arestas[node].g_fim
+
+                #
                 if not filho in self.visitado:
                     self.resultado.append((current_node, filho))
                     frontier.put(
@@ -224,29 +265,42 @@ class BuscaGreedy(Buscas):
 
 
 class BuscaAEstrela(Buscas):
+    # Variaveis que serão utilizadas durante a busca
     def __init__(self):
         super().__init__()
+        # Nome da busca
         self.name = "Busca A* (A Estrela)"
         self.came_from = {}
         self.cost_so_far = {}
 
+
     def search(self, data, estado_pai):
         frontier = PriorityQueue()
         frontier.put((0, estado_pai))
-
+        
         self.cost_so_far[estado_pai] = 0
 
+        # Se minha lista de pais não for vazia, entra no while
         while not frontier.empty():
             ucs_w, current = frontier.get_nowait()
+            # adiciona o atual estado a ser analizado na lista de visitados
             self.visitado.append(current)
-
+            
+            # Se o estado atual for o estado final, sai do while
             if current.goal:
                 # print("Cheguei no final! ", current_node)
                 return
 
+            # Para o proximo estado na lista de filhos
             for next in current.children:
+                # Ele recebera um novo custo sendo este custo a soma da distancia ja andada para chegar até ele, 
+                # Mais a heuristica dele mesmo até o final
                 new_cost = ucs_w + current.arestas[next].custo
+                #
                 filho = current.arestas[next].g_fim
+                
+                # se o custo do proximo filho a ser comparado não for maior que o do filho analizado anteriormente, 
+                # ou o novo custo for menor que a distancia que ele ja percorreu ele entra no if
                 if next not in self.cost_so_far or new_cost < ucs_w:
                     priority = new_cost + current.arestas[next].custoH
                     self.resultado.append((current, filho))
